46c84918c8c257e3336a0fd3d2f653b1cd3ad132
Jonathan Luo <67038929+Jonathan-Luo01@users.noreply.github.com>
2023-07-12 15:48:17 -0700

    Update mic.py

--- mic.py
+++ mic.py
@@ -16,9 +16,9 @@ class Microphone():
 		self.format = pyaudio.paInt16
 		self.audio_filename = 'audio.wav'
 		self.audio = pyaudio.PyAudio()
-		#These parameters are different for each audio device.
+		# These parameters are different for each audio device.
 
-		#The following code reveals the parameters of your own device.
+		# The following code reveals the parameters of your own device.
   		'''
 		for i in range(self.audio.get_device_count()):
 			print(self.audio.get_device_info_by_index(i))
@@ -41,7 +41,7 @@ class Microphone():
 
 		self.stream.stop_stream()
 
-    #create audio file
+    # create audio file
 		waveFile = wave.open(self.audio_filename, 'wb')
 		waveFile.setnchannels(self.channels)
 		waveFile.setsampwidth(self.audio.get_sample_size(self.format))
